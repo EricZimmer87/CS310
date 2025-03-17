2ff55320aeacef5aff2791d33779ee6dd942d9ed
EricZimmer87 <ericzimmer87@icloud.com>
2025-03-17 10:49:52 -0500

    Added input validation.

--- Ch03Exercise07/Ch03Exercise07.cpp
+++ Ch03Exercise07/Ch03Exercise07.cpp
@@ -1,6 +1,6 @@
 /*
 Student's Name: Eric Zimmer
-Date: 3/13/2025
+Date: 3/17/2025
 File Name: Ch03Exercise07.cpp
 Description: This program asks the user for the net balance,
 payment, number of days in the billing cycle, number of days
@@ -19,27 +19,48 @@ int main() {
     // Get net balance
     double netBalance;
     cout << "What is the net balance: " << endl;
-    cin >> netBalance;
+    // Keep asking for input until input is valid
+    while (!(cin >> netBalance) || netBalance < 0) {
+        cin.clear(); // Clear error flag
+        cin.ignore(1000, '\n'); // Ignore invalid input
+        cout << "Invalid input. Please enter a valid number greater than or equal to 0:" << endl;
+    }
 
     // Get payment
     double payment;
-    cout << "What is the payment: " << endl;
-    cin >> payment;
+    cout << "What is the payment:" << endl;
+    while (!(cin >> payment) || payment < 0) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Please enter a valid number greater than or equal to 0:" << endl;
+    }
 
     // Get number of days in the billing cycle (d1)
     int d1;
-    cout << "What is the number of days in the billing cycle: " << endl;
-    cin >> d1;
+    cout << "What is the number of days in the billing cycle:" << endl;
+    while (!(cin >> d1) || d1 <= 0) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Please enter a valid number greater than 0:" << endl;
+    }
 
     // Get number of days payment is made before the billing cycle (d2)
     int d2;
-    cout << "What is the number of days payment is made before the billing cycle: " << endl;
-    cin >> d2;
+    cout << "What is the number of days payment is made before the billing cycle:" << endl;
+    while (!(cin >> d2) || d2 <= 0) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Please enter a valid number greater than 0:" << endl;
+    }
 
     // Get interest rate per month
     double interestRate;
-    cout << "What is the interest rate per month: " << endl;
-    cin >> interestRate;
+    cout << "What is the interest rate per month:" << endl;
+    while(!(cin >> interestRate) || interestRate < 0) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Please enter a valid number greater than or equal to 0:" << endl;
+    }
 
     // Calculate the average daily balance
     double averageDailyBalance = (netBalance * d1 - payment * d2) / d1;
