5f7124725316a1c4abf22f6e4b56e214a940e4b9
Eric Zimmer <ericzimmer87@icloud.com>
2025-04-25 11:11:19 -0500

    Added John Barron's corrected program for M7: Discussion.

---
+++ M7_Discussion/John_Barron_M7_Discussion.cpp
@@ -0,0 +1,51 @@
+/*
+Student's Name: Eric Zimmer
+Course:         CS310-T301 Programming with C++
+Assignment:     M7: Discussion
+Professor:      Dr. Robert Flowers
+Date:           04/25/2025
+File Name:      John_Barron_M7_Discussion.cpp
+Description:    
+
+Developed and tested in Visual Studio 2022 (Windows 11).
+Compiled with Microsoft (R) C/C++ Optimizing Compiler Version 19.43.34810.
+*/
+
+/* Created by John-Michael
+Programming with C++ (2D arrays)
+Program should display a gridded maze, 7 across, 5 down as a rectangle.
+hint: my endl; seems a bit impatient. 2 errors, 1 formatting flaw */
+
+#include <iostream>
+using namespace std;
+
+const int row = 5;
+const int col = 7;
+
+int maze[row][col] = {
+    {0, 1, 0, 0, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0},
+    {0, 0, 0, 1, 0, 0, 0},
+    {1, 1, 0, 1, 1, 1, 0},
+    {1, 1, 0, 0, 0, 0, 0}
+}; // Semicolon was missing -EZ
+
+void displayTheMaze() {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            if (maze[i][j] == 1) {
+                cout << "#";
+            }
+            else {
+                cout << ".";
+            }
+        }
+        cout << endl; // Moved this to the outer loop -EZ
+    }
+}
+
+int main() {
+    cout << "This is my maze hope ya like it!\n";
+    displayTheMaze(); // Corrected the function name -EZ
+    return 0;
+}
